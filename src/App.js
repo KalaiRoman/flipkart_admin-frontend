61e81e628a637f73bc316a7317756a56e9670b75
kalai <kalai@cdp360.com>
2024-03-13 00:04:20 +0530

    admin folders

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      admin app kalai amma app
+    </div>
+  );
+}
+
+export default App;
